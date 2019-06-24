ae344651deb7e8353ffceaf551ac4cc0f09b79de
Takanao Ishimura <ishimurataki@gmail.com>
2019-06-24 11:05:08 -0400

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,37 @@
+console.log('Client side javascript file is loaded');
+
+fetch('http://puzzle.mead.io/puzzle').then((response) => {
+    response.json().then((data) => {
+        console.log(data);
+    })
+})
+
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const location = search.value;
+    const url = 'http://localhost:3000/weather?address=' + encodeURIComponent(location);
+
+    messageOne.textContent = 'Loading...';
+    messageTwo.textContent = ' ';
+
+    fetch(url).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                return messageOne.textContent = data.error;
+            }
+            const message = data.forecast.summary + " It is currently " + 
+                data.forecast.temperature + " degrees out. " +
+                "There is a " + data.forecast.precipProbability + "% chance of rain.";
+                
+            messageOne.textContent = data.location;
+            messageTwo.textContent = message;
+            console.log(data.forecast);
+        })
+    })
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,20 @@
+const request = require('request');
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'https://api.darksky.net/forecast/ad7027b8ef1cbd936f90b3b04d7369fb/' + latitude + ',' + longitude;
+    request({url, json : true}, (error, {body}) => {
+        if (error) {
+            callback('Unable to connect to weather services!', undefined);
+        } else if (body.error) {
+            callback('Unable to find location!', undefined);
+        } else {
+            callback(undefined, {
+                summary : body.daily.data[0].summary,
+                temperature : body.currently.temperature,
+                precipProbability : body.currently.precipProbability
+            })
+        }
+    });
+}
+
+module.exports = forecast;
